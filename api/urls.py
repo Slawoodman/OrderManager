7af9c0fa42c59ecbf5634be24f6fd91c637af7c3
Slawoodman <Slawoodmen@gmail.com>
2023-06-13 00:11:36 +0300

    modified:   OrderManager/__pycache__/__init__.cpython-310.pyc 	modified:   OrderManager/__pycache__/settings.cpython-310.pyc 	modified:   OrderManager/__pycache__/urls.cpython-310.pyc 	modified:   OrderManager/__pycache__/wsgi.cpython-310.pyc 	modified:   api/__pycache__/__init__.cpython-310.pyc 	modified:   api/__pycache__/serializers.cpython-310.pyc 	modified:   api/__pycache__/urls.cpython-310.pyc 	modified:   api/__pycache__/utils.cpython-310.pyc 	modified:   api/__pycache__/views.cpython-310.pyc 	modified:   api/urls.py 	modified:   api/views.py 	modified:   db.sqlite3 	modified:   market/__pycache__/__init__.cpython-310.pyc 	modified:   market/__pycache__/admin.cpython-310.pyc 	modified:   market/__pycache__/apps.cpython-310.pyc 	modified:   market/__pycache__/forms.cpython-310.pyc 	modified:   market/__pycache__/models.cpython-310.pyc 	modified:   market/__pycache__/urls.cpython-310.pyc 	modified:   market/__pycache__/utils.cpython-310.pyc 	modified:   market/__pycache__/views.cpython-310.pyc 	modified:   market/migrations/__pycache__/0001_initial.cpython-310.pyc 	modified:   market/migrations/__pycache__/0002_initial.cpython-310.pyc 	modified:   market/migrations/__pycache__/0003_alter_product_created_date.cpython-310.pyc 	modified:   market/migrations/__pycache__/0004_alter_product_created_date.cpython-310.pyc 	modified:   market/migrations/__pycache__/0005_alter_product_created_date.cpython-310.pyc 	modified:   market/migrations/__pycache__/0006_remove_product_created_date_product_created.cpython-310.pyc 	modified:   market/migrations/__pycache__/0007_orderitem_is_paid.cpython-310.pyc 	modified:   market/migrations/__pycache__/__init__.cpython-310.pyc 	new file:   static/media/payment_8xlX0Ug.html 	new file:   static/media/payment_sXGnEj4.html 	modified:   users/__pycache__/__init__.cpython-310.pyc 	modified:   users/__pycache__/admin.cpython-310.pyc 	modified:   users/__pycache__/apps.cpython-310.pyc 	modified:   users/__pycache__/forms.cpython-310.pyc 	modified:   users/__pycache__/models.cpython-310.pyc 	modified:   users/__pycache__/urls.cpython-310.pyc 	modified:   users/__pycache__/views.cpython-310.pyc 	modified:   users/migrations/__pycache__/0001_initial.cpython-310.pyc 	modified:   users/migrations/__pycache__/__init__.cpython-310.pyc

--- api/urls.py
+++ api/urls.py
@@ -8,15 +8,17 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path("", views.getRoutes),
-    path("products/", views.getProducts),
-    path("products/<str:pk>", views.getProduct),
-    path("products/<str:pk>/order/create/", views.createOrder),
-    path("orders/", views.getOrders),
-    path("orders/<str:pk>", views.getOrder),
-    path("orders/<str:pk>/payment", views.orderPayment),
-    path("orders/<str:pk>/payment/check", views.orderGenBill),
-    path("orders/<str:pk>/changestatus", views.changeOrderStatus),
+    path("routes/", views.RoutesAPIView.as_view(), name="routes"),
+    path("products/", views.ProductsAPIView.as_view(), name="products"),
+    path("products/<int:pk>/", views.ProductAPIView.as_view(), name="product"),
+    path("products/<int:pk>/order/create/", views.CreateOrderAPIView.as_view(), name="create_order"),
+    
+    path("orders/", views.OrdersAPIView.as_view(), name="orders"),
+    path("orders/<int:pk>/", views.OrderAPIView.as_view(), name="order"),
+    path("orders/<int:pk>/payment/", views.OrderPaymentAPIView.as_view(), name="order_payment"),
+    path("orders/<int:pk>/gen-bill/", views.OrderGenBillAPIView.as_view(), name="order_gen_bill"),
+    path("orders/<int:pk>/change-status/", views.ChangeOrderStatusAPIView.as_view(), name="change_order_status"),
+
     path("users/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("users/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.decorators import APIView
+from rest_framework.permissions import IsAuthenticated
 from django.template.loader import render_to_string
 from django.core.files.base import ContentFile
 from django.shortcuts import get_object_or_404
@@ -17,125 +17,138 @@ from .utils import filter_orders_by_role
 
 
 
-@api_view(["GET"])
-def getRoutes(request):
-    routes = [
-        {"GET": "/api/products"},
-        {"GET": "/api/orders"},
-        {"POST": "/api/users/token"},
-        {"POST": "/api/users/token/refresh"},
-    ]
-    return Response(routes)
+class RoutesAPIView(APIView):
+    def get(self, request):
+        routes = [
+            {"GET": "/api/routes/"},
+            {"GET": "/api/products/"},
+            {"GET": "/api/products/<int:pk>/"},
+            {"GET": "/api/orders/"},
+            {"GET": "/api/orders/<int:pk>/"},
+            {"POST": "/api/products/<int:pk>/order/create/"},
+            {"POST": "/api/orders/<int:pk>/payment/"},
+            {"POST": "/api/orders/<int:pk>/gen-bill/"},
+            {"POST": "/api/orders/<int:pk>/change-status/"},
+            {"POST": "/api/users/token/"},
+            {"POST": "/api/users/token/refresh/"},
+
+        ]
+        return Response(routes)
 
 
-@api_view(["GET"])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+class ProductsAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
 
-@api_view(["GET"])
-def getProduct(request, pk):
-    product = Product.objects.get(id=pk)
-    serializer = ProductSerializer(product, many=False)
-    return Response(serializer.data)
+class ProductAPIView(APIView):
+    def get(self, request, pk):
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product, many=False)
+        return Response(serializer.data)
 
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def getOrders(request):
-    user = request.user
-    orders = filter_orders_by_role(user)
-    serializer = OrderItemSerializer(orders, many=True, context={"request": request})
-    return Response(serializer.data)
+class OrdersAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        orders = filter_orders_by_role(user)
+        serializer = OrderItemSerializer(orders, many=True, context={"request": request})
+        return Response(serializer.data)
 
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def getOrder(request, pk):
-    orders = OrderItem.objects.get(id=pk)
-    serializer = OrderItemSerializer(orders, many=False)
-    return Response(serializer.data)
+class OrderAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        order = OrderItem.objects.get(id=pk)
+        serializer = OrderItemSerializer(order, many=False)
+        return Response(serializer.data)
 
 
-@api_view(["POST"])
-@permission_classes([IsAuthenticated])
-def createOrder(request, pk):
-    user = request.user
-    role = user.role
-    item = Product.objects.get(id=pk)
+class CreateOrderAPIView(APIView):
+    permission_classes = [IsAuthenticated]
 
-    if role == User.Role.USER:
-        form = OrderCreatForm(request.data)
+    def post(self, request, pk):
+        user = request.user
+        role = user.role
+        item = Product.objects.get(id=pk)
 
-        if form.is_valid():
+        if role == User.Role.USER:
+            form = OrderCreatForm(request.data)
 
-            new_order = form.save(commit=False)
-            new_order.customer = user
-            new_order.product = item
-            new_order.save()
+            if form.is_valid():
+                new_order = form.save(commit=False)
+                new_order.customer = user
+                new_order.product = item
+                new_order.save()
 
-            return Response(
-                "New order created successfully!", status=status.HTTP_201_CREATED
-            )
+                return Response(
+                    "New order created successfully!", status=status.HTTP_201_CREATED
+                )
+            else:
+                return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
-    else:
+            return Response(
+                "Only users can create a new order.", status=status.HTTP_403_FORBIDDEN
+            )
+
+
+class OrderPaymentAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        order_item = get_object_or_404(OrderItem, id=pk)
+        user = request.user
+        role = user.role
+
+        if role == User.Role.USER and order_item.customer == user:
+            try:
+                order_item.paid()
+                return Response("U've paid for the order.", status=status.HTTP_200_OK)
+            except:
+                return Response("An error occurred.", status=status.HTTP_400_BAD_REQUEST)
         return Response(
-            "Only users can create a new order.", status=status.HTTP_403_FORBIDDEN
+            "Only owners can pay for the order.", status=status.HTTP_403_FORBIDDEN
         )
 
 
-@api_view(["POST"])
-@permission_classes([IsAuthenticated])
-def orderPayment(request, pk):
-    order_item = get_object_or_404(OrderItem, id=pk)
-    user = request.user
-    role = user.role
+class OrderGenBillAPIView(APIView):
+    permission_classes = [IsAuthenticated]
 
-    if role == User.Role.USER and order_item.customer == user:
-        try:
-            order_item.paid()
-            return Response("U've paid for the order.", status=status.HTTP_200_OK)
-        except:
-            return Response("An error occurred.", status=status.HTTP_400_BAD_REQUES)
-    return Response(
-        "Only owners can pay for the order.", status=status.HTTP_403_FORBIDDEN
-    )
+    def post(self, request, pk):
+        order_item = get_object_or_404(OrderItem, id=pk)
+        user = request.user
 
+        if request.user.role == User.Role.CASHIER:
+            if order_item.is_paid:
+                file_name = "payment.html"
+                context = {
+                    "order_item": order_item,
+                }
+                html_content = render_to_string("market/payment_template.html", context)
 
-@api_view(["POST"])
-@permission_classes([IsAuthenticated])
-def orderGenBill(request, pk):
-    order_item = get_object_or_404(OrderItem, id=pk)
-    user = request.user
+                # Save the HTML content to the OrderItem instance
+                order_item.file.save(file_name, ContentFile(html_content), save=True)
+                return Response("Payment is created...", status=status.HTTP_200_OK)
+            return Response(
+                "Wait for the user to pay for the order.", status=status.HTTP_400_BAD_REQUEST
+            )
 
-    if request.user.role == User.Role.CASHIER:
-        if order_item.is_paid:
-            file_name = "payment.html"
-            context = {
-                "order_item": order_item,
-            }
-            html_content = render_to_string("market/payment_template.html", context)
-
-            # Save the HTML content to the OrderItem instance
-            order_item.file.save(file_name, ContentFile(html_content), save=True)
-            return Response("Payment is created...", status=status.HTTP_200_OK)
-        return Response(
-            "Wait for the user to pay for the order.", status=status.HTTP_400_BAD_REQUES
-        )
-
-    return Response("U shall not pass!!!", status=status.HTTP_403_FORBIDDEN)
+        return Response("U shall not pass!!!", status=status.HTTP_403_FORBIDDEN)
 
 
-@api_view(["POST"])
-@permission_classes([IsAuthenticated])
-def changeOrderStatus(request, pk):
-    order = get_object_or_404(OrderItem, id=pk)
-    status = request.data.get("status")
-    if status in dict(OrderItem.STATUS_CHOICES):
-        order.status = status
-        order.save()
-        return Response("Order status changed successfully!")
-    return Response("Invalid status value.")
+class ChangeOrderStatusAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        order = get_object_or_404(OrderItem, id=pk)
+        status = request.data.get("status")
+        if status in dict(OrderItem.STATUS_CHOICES):
+            order.status = status
+            order.save()
+            return Response("Order status changed successfully!")
+        return Response("Invalid status value.")
