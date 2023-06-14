f5251d364d76882e1d4554a8f42599b37c29de9e
Slawoodman <Slawoodmen@gmail.com>
2023-06-15 01:45:48 +0300

    new file:   Ordercollections.postman_collection.json 	modified:   db.sqlite3 	modified:   market/__pycache__/models.cpython-310.pyc 	new file:   market/migrations/0008_alter_orderitem_address_alter_orderitem_city_and_more.py 	new file:   market/migrations/0009_alter_orderitem_address_alter_orderitem_city_and_more.py 	new file:   market/migrations/__pycache__/0008_alter_orderitem_address_alter_orderitem_city_and_more.cpython-310.pyc 	new file:   market/migrations/__pycache__/0009_alter_orderitem_address_alter_orderitem_city_and_more.cpython-310.pyc 	modified:   market/models.py 	modified:   market/templates/market/main.html 	new file:   products.json 	new file:   requirements.txt 	new file:   static/media/payment_c9THKA1.html 	new file:   static/media/payment_dlOl4VI.html 	modified:   tests/__pycache__/test_api.cpython-310.pyc 	modified:   tests/__pycache__/test_market.cpython-310.pyc 	modified:   tests/__pycache__/test_users.cpython-310.pyc 	modified:   tests/test_api.py 	modified:   tests/test_market.py 	modified:   tests/test_users.py

--- market/models.py
+++ market/models.py
@@ -28,7 +28,6 @@ class Product(models.Model):
         super().save(*args, **kwargs)
 
 
-# продукт цена, юзера айди, статутс.
 class OrderItem(models.Model):
     STATUS_CHOICES = (
         ("Undecided", "UNDECIDED"),
@@ -38,9 +37,9 @@ class OrderItem(models.Model):
 
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    address = models.CharField(max_length=250, blank=True, null=True)
-    postal_code = models.CharField(max_length=20, blank=True, null=True)
-    city = models.CharField(max_length=100, blank=True, null=True)
+    address = models.CharField(max_length=250, default="")
+    postal_code = models.CharField(max_length=20, default="")
+    city = models.CharField(max_length=100, default="")
     created = models.DateTimeField(
         auto_now_add=False, blank=True, null=True, default=timezone.now
     )
